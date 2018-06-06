ae5f732344410877bc40f68b8108f4ea61fa0078
John Raber <jrab224@gmail.com>
2018-06-06 00:30:57 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,13 +1,9 @@
 def my_select(collection)
   i = 0
  
-  if collection.size > 0
-   
-    while i < collection.size
-      yield collection[i]
-      i += 1
-    end
-    
+  while i < collection.size
+    yield collection[i]
+    i += 1
   end
   
 end
